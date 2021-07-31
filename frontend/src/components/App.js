3bf555c72d63e2a69d1261b6387729b85a56bd84
Winfred Huang <huang.winfred21@gmail.com>
2021-07-30 23:49:48 -0700

    Remove comments from App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,7 +5,6 @@ import {
   Route,
   Link
 } from 'react-router-dom';
-// import logo from './logo.svg';
 
 import Home from './Home';
 import Direction from './Direction';
@@ -51,25 +50,6 @@ function App() {
       </div>
     </Router>
   );
-
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
